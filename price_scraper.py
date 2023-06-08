d9b8c6f5283f4710696f585f9a6f82195568498b
ovadaprofit <110870807+ovadaprofit@users.noreply.github.com>
2023-06-08 16:38:13 +0100

    price scraper - sys_setup 삭제
    requirements.txt - urllib3 version 수정
    
    price_scraper.py
    - get_data 함수 추가
    - tickers를 엑셀 파일에 있는 것으로 바꿈
    print(tickers) 할 시에 제대로 나온 것 확인함

--- price_scraper.py
+++ price_scraper.py
@@ -2,9 +2,38 @@ import logging
 from prep import Prep
 import yfinance as yf
 
+import openpyxl
+import pandas as pd
+import os
+
+def get_data (file, sheet, start_row, end_row, column_index):
+    df = pd.read_excel(file, sheet_name=sheet)
+
+    data = []
+
+    for row in range(start_row, end_row+1):
+        value = df.iloc[row-1,column_index]
+        value = value.rstrip(" (LSE)") # 뒤에 달린 (LSE) 삭제
+        data.append(value)
+
+    return data
+
+#df1 = pd.read_excel('/Users/yoonsanglee/Documents/GitHub/Summer2023/files/SPGlobal_ListManager-All_27-May-2023.xlsx')
+file = '/Users/yoonsanglee/Documents/GitHub/Summer2023/files/SPGlobal_ListManager-All_27-May-2023.xlsx'
+sheet = 'List Manager - Companies'
+start_row = 8
+end_row = 662
+column_index = 1
+
+tickers = get_data(file, sheet, start_row, end_row, column_index)
+
+#print(tickers)
+
 
 # TODO : extract all tickers from the .xlsx file
-tickers = ["AZN", "HSBA", "SHEL", "OKYO"]
+# (lse) 빼고 찾기
+# tickers = ["AZN", "HSBA", "SHEL", "OKYO"]
+# tickers = ["ADMR", "AAF"]
 period = "max"
 
 for ticker in tickers:
@@ -15,4 +44,3 @@ for ticker in tickers:
     history = history.round(3)
     logging.info(f"{ticker} data completed")
     history.to_csv(f"./files/history/{ticker}.csv")
-
