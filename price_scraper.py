d55b56ffb0b422c56af0b246918502d397aa4747
ovadaprofit <110870807+ovadaprofit@users.noreply.github.com>
2023-06-09 11:34:22 +0100

    price scraper - sys_setup 삭제
    requirements.txt - urllib3 version 수정
    
    price_scraper.py (June 8)
    - get_data 함수 추가
    - tickers를 엑셀 파일에 있는 것으로 바꿈
    print(tickers) 할 시에 제대로 나온 것 확인함
    
    price_scraper.py (June 9)
    - 맨뒤에 . 있는 경우 삭제
    - BT.A처럼 중간에 . 있는 경우 -로 변경 (BT-A)
    - 에러는 엑셀 확인 바람

--- price_scraper.py
+++ price_scraper.py
@@ -6,6 +6,7 @@ import openpyxl
 import pandas as pd
 import os
 
+# 엑셀 종목들을 tickers라는 list로
 def get_data (file, sheet, start_row, end_row, column_index):
     df = pd.read_excel(file, sheet_name=sheet)
 
@@ -13,7 +14,7 @@ def get_data (file, sheet, start_row, end_row, column_index):
 
     for row in range(start_row, end_row+1):
         value = df.iloc[row-1,column_index]
-        value = value.rstrip(" (LSE)") # 뒤에 달린 (LSE) 삭제
+        value = value.replace(" (LSE)", "").rstrip(".").replace(".","-")
         data.append(value)
 
     return data
@@ -29,6 +30,16 @@ tickers = get_data(file, sheet, start_row, end_row, column_index)
 
 #print(tickers)
 
+# tickers list를 csv로
+file_path = "tickers.csv"
+
+df = pd.DataFrame(tickers, columns=["Ticker"])
+
+df.to_csv(f"./files/history/*tickers.csv")
+
+print("Tickers written to CSV file.")
+
+
 
 # TODO : extract all tickers from the .xlsx file
 # (lse) 빼고 찾기
@@ -40,7 +51,7 @@ for ticker in tickers:
     equity = yf.Ticker(ticker+".L")
 
     # df of columns Open/High/Low/Close/Volume/Dividends/Stock Splits
-    history = equity.history(period=period, end="2022-12-31")
+    history = equity.history(period=period, end="2023-06-20")
     history = history.round(3)
     logging.info(f"{ticker} data completed")
     history.to_csv(f"./files/history/{ticker}.csv")
