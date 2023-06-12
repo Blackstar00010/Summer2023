bfc74163dfe4de8069b4bf3e9e2229330ef54fa9
ovadaprofit <110870807+ovadaprofit@users.noreply.github.com>
2023-06-12 19:41:02 +0100

    price scraper - sys_setup 삭제
    requirements.txt - urllib3 version 수정
    
    price_scraper.py (June 8)
    - get_data 함수 추가
    - tickers를 엑셀 파일에 있는 것으로 바꿈
    print(tickers) 할 시에 제대로 나온 것 확인함
    
    price_scraper.py (June 9)
    - 맨뒤에 . 있는 경우 삭제
    - BT.A처럼 중간에 . 있는 경우 -로 변경 (BT-A)
    - 에러는 엑셀 확인 바람
    
    csv_merge.py
    - 아직 에러 있음 (고칠 예정)

---
+++ csv_merge.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+
+directory = './files/history'
+csv_history = sorted(filename for filename in os.listdir(directory))
+
+merged_data = pd.DataFrame()
+
+for file in csv_history:
+    file_path = os.path.join(directory, file)
+    df = pd.read_csv(file_path)
+    if 'Date' in df.columns and 'Close' in df.columns:
+        df = df[['Date', 'Close']]
+        if merged_data.empty:
+            merged_data = df
+        else:
+            merged_data = pd.merge(merged_data, df, on='Date', how='outer')
+
+merged_data.to_csv('./files/merged_data.csv', index=False)
+
+
+'''
+import pandas as pd
+
+df = pd.read_csv('./files/history/888.csv')
+var = df[["Date", "Close"]]
+
+files = './files/history/*'
+'''
+
+
+
+'''
+from price_scraper import tickers
+
+for ticker in tickers:
+    files = f'./files/history/{ticker}.csv'
+'''
+
