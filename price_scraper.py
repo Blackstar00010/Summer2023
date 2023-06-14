0c61721f835473a28ab772271588ef8f30a0a02b
ovadaprofit <110870807+ovadaprofit@users.noreply.github.com>
2023-06-14 10:51:13 +0100

    price scraper - sys_setup 삭제
    requirements.txt - urllib3 version 수정
    
    price_scraper.py (June 8)
    - get_data 함수 추가
    - tickers를 엑셀 파일에 있는 것으로 바꿈
    print(tickers) 할 시에 제대로 나온 것 확인함
    
    price_scraper.py (June 9)
    - 맨뒤에 . 있는 경우 삭제
    - BT.A처럼 중간에 . 있는 경우 -로 변경 (BT-A)
    - 에러는 엑셀 확인 바람
    
    csv_merge.py (June 12)
    - 아직 에러 있음 (고칠 예정)
    
    csv_merge.py (June 13)
    - 에러 수정함, 다만 맞는지 점검 필요함
    
    csv_merge.py (June 14)
    - 날짜 순서가 이상해서 날짜 sorting 코드 추가

--- price_scraper.py
+++ price_scraper.py
@@ -1,23 +1,24 @@
 import logging
+from prep import Prep
 import yfinance as yf
+
+import openpyxl
 import pandas as pd
+import os
 
-
-# 엑셀 종목들을 tickers라는 list로
-def get_data(file, sheet, start_row, end_row, column_index):
+def get_data (file, sheet, start_row, end_row, column_index):
     df = pd.read_excel(file, sheet_name=sheet)
 
     data = []
 
-    for row in range(start_row, end_row + 1):
-        value = df.iloc[row - 1, column_index]
-        value = value.replace(" (LSE)", "").rstrip(".").replace(".", "-")
+    for row in range(start_row, end_row+1):
+        value = df.iloc[row-1,column_index]
+        value = value.replace(" (LSE)", "").rstrip(".").replace(".","-")
         data.append(value)
 
     return data
 
 
-# df1 = pd.read_excel('/Users/yoonsanglee/Documents/GitHub/Summer2023/files/SPGlobal_ListManager-All_27-May-2023.xlsx')
 file = './files/SPGlobal_ListManager-All_27-May-2023.xlsx'
 sheet = 'List Manager - Companies'
 start_row = 8
@@ -26,7 +27,6 @@ column_index = 1
 
 tickers = get_data(file, sheet, start_row, end_row, column_index)
 
-# tickers list를 csv로
 file_path = "tickers.csv"
 
 df = pd.DataFrame(tickers, columns=["Ticker"])
@@ -35,15 +35,11 @@ df.to_csv(f"./files/history/*tickers.csv")
 
 print("Tickers written to CSV file.")
 
-# (lse) 빼고 찾기
-# tickers = ["AZN", "HSBA", "SHEL", "OKYO"]
-# tickers = ["ADMR", "AAF"]
 period = "max"
 
 for ticker in tickers:
-    equity = yf.Ticker(ticker + ".L")
+    equity = yf.Ticker(ticker+".L")
 
-    # df of columns Open/High/Low/Close/Volume/Dividends/Stock Splits
     history = equity.history(period=period, end="2023-06-20")
     history = history.round(3)
     logging.info(f"{ticker} data completed")
