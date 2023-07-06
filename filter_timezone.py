9fd30c7f6c68bfbc20af89dfef73c102a4cc3cb7
Blackstar00010 <djwjames1@gmail.com>
2023-07-06 15:42:21 +0900

    added month start flag

--- filter_timezone.py
+++ filter_timezone.py
@@ -9,7 +9,18 @@ df_filtered = df[df['Date'].str.contains('00:00:00\+00:00|00:00:00\+01:00')]
 # Save the filtered data to a new CSV file
 df_filtered.to_csv('./files/filtered_price_data.csv', index=False)
 
+"""
+import pandas as pd
 
+dir = "./files/"
+
+df = pd.read_csv(dir+"filtered_price_data.csv")
+dates = df['Date']
+months = pd.DataFrame([item[5:7] for item in dates])
+flags = (months == months.shift(1)).dropna()
+df["Month Start Flag"] = flags
+df.to_csv(dir+"asdf.csv")
+"""
 
 
 
