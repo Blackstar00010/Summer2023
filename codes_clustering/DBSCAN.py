6b2604d9b1dfa7a8024d8a8eee1c77fea9a79331
ovadaprofit <110870807+ovadaprofit@users.noreply.github.com>
2023-07-07 14:21:45 +0900

    price scraper - sys_setup 삭제
    requirements.txt - urllib3 version 수정
    
    price_scraper.py (June 8)
    - get_data 함수 추가
    - tickers를 엑셀 파일에 있는 것으로 바꿈
    print(tickers) 할 시에 제대로 나온 것 확인함
    
    price_scraper.py (June 9)
    - 맨뒤에 . 있는 경우 삭제
    - BT.A처럼 중간에 . 있는 경우 -로 변경 (BT-A)
    - 에러는 엑셀 확인 바람
    
    csv_merge.py (June 12)
    - 아직 에러 있음 (고칠 예정)
    
    csv_merge.py (June 13)
    - 에러 수정함, 다만 맞는지 점검 필요함
    
    csv_merge.py (June 14)
    - 날짜 순서가 이상해서 날짜 sorting 코드 추가
    
    csv_to_sql (June 17)
    - 일단 작성하였는데 아직 sql 여는 방법을 잘 모르겠음
    
    delisted 관련 (June 24)
    - Delisted Tickers
    - Delisted ISIN
    - Delisted Price는 못 함
    (compustat 사용 혹은 유료)
    
    K-Means Using SciKit (July 5)
    - graph version 추가

--- codes_clustering/DBSCAN.py
+++ codes_clustering/DBSCAN.py
@@ -1,48 +1,50 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
 
 # Read data from CSV file
 data = pd.read_csv('../files/feature_set/1990-1.csv')
 data_array = data.values[:, 1:]  # Exclude the first column (firm names)
 
+# Define DBSCAN parameters
+eps = 80  # Maximum distance between two samples to be considered as neighbors
+min_samples = 3  # Minimum number of samples in a neighborhood for a point to be considered as a core point
+
 # Perform DBSCAN clustering
-clusters_DBSCAN = {}
+dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+cluster_labels = dbscan.fit_predict(data_array)
 
-dbscan = DBSCAN(eps=0.5, min_samples=5)  # Set your own parameters
-dbscan.fit(data_array)  # Compute DBSCAN clustering
-cluster_labels = dbscan.labels_  # Label of each point
+# Get the unique cluster labels
+unique_labels = set(cluster_labels)
 
-n_clusters = len(set(cluster_labels)) - (1 if -1 in cluster_labels else 0)  # Number of clusters, excluding noise if present
+# Create a dictionary to store firms in each cluster
+clusters = {label: [] for label in unique_labels}
 
-clusters_DBSCAN = {i: [] for i in range(n_clusters)}  # Dictionary key-value pair
-for i, cluster in enumerate(cluster_labels):
-    if cluster != -1:  # Exclude noise points
-        clusters_DBSCAN[cluster].append(f'firm {i + 1}')
+# Group firms by cluster label
+for i, cluster_label in enumerate(cluster_labels):
+    clusters[cluster_label].append(f'firm {i + 1}')
 
-# Print the clusters for each k value
-for k, clusters in clusters_DBSCAN.items():
-    print(f'Clusters for k = {k}:')
-    for cluster, firms in clusters.items():
-        print(f'Cluster {cluster + 1}: {firms}')
+# Print the clusters
+for cluster_label, firms in clusters.items():
+    print(f'Cluster {cluster_label}: {firms}')
 
-        # PLot the line graph for firms in each cluster
-        for firm in firms:
-            firm_index = int(firm.split()[1]) - 1  # Extract firm number from firm name
-            firm_data = data_array[firm_index]
+    # Plot the line graph for firms in the cluster
+    for firm in firms:
+        firm_index = int(firm.split()[1]) - 1  # Extract firm number from firm name
+        firm_data = data_array[firm_index]
 
-            plt.plot(range(1, len(firm_data) + 1), firm_data, label=firm)
+        plt.plot(range(1, len(firm_data) + 1), firm_data, label=firm)
 
-        plt.xlabel('Characteristics')
-        plt.ylabel('Data Value')
-        plt.title(f'Cluster {cluster + 1}, k={k}')
+    plt.xlabel('Characteristics')
+    plt.ylabel('Data Value')
+    plt.title(f'Cluster {cluster_label}')
 
-        # List the firm names on the side of the graph
-        if len(firms) <= 10:
-            plt.legend(loc='center right')
-        else:
-            plt.legend(loc='center right', title=f'Total Firms: {len(firms)}', labels=firms[:10] + ['...'])
+    # List the firm names on the side of the graph
+    if len(firms) <= 10:
+        plt.legend(loc='center right')
+    else:
+        plt.legend(loc='center right', title=f'Total Firms: {len(firms)}', labels=firms[:10] + ['...'])
 
-        plt.show()
+    plt.show()
 
     print()
