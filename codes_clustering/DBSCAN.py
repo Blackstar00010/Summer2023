f505f3ecda53f5d55eb993b05f7e0e0e60378844
ovadaprofit <110870807+ovadaprofit@users.noreply.github.com>
2023-07-10 17:55:28 +0900

    price scraper - sys_setup 삭제
    requirements.txt - urllib3 version 수정
    
    price_scraper.py (June 8)
    - get_data 함수 추가
    - tickers를 엑셀 파일에 있는 것으로 바꿈
    print(tickers) 할 시에 제대로 나온 것 확인함
    
    price_scraper.py (June 9)
    - 맨뒤에 . 있는 경우 삭제
    - BT.A처럼 중간에 . 있는 경우 -로 변경 (BT-A)
    - 에러는 엑셀 확인 바람
    
    csv_merge.py (June 12)
    - 아직 에러 있음 (고칠 예정)
    
    csv_merge.py (June 13)
    - 에러 수정함, 다만 맞는지 점검 필요함
    
    csv_merge.py (June 14)
    - 날짜 순서가 이상해서 날짜 sorting 코드 추가
    
    csv_to_sql (June 17)
    - 일단 작성하였는데 아직 sql 여는 방법을 잘 모르겠음
    
    delisted 관련 (June 24)
    - Delisted Tickers
    - Delisted ISIN
    - Delisted Price는 못 함
    (compustat 사용 혹은 유료)
    
    K-Means Using SciKit (July 5)
    - graph version 추가

--- codes_clustering/DBSCAN.py
+++ codes_clustering/DBSCAN.py
@@ -23,7 +23,7 @@ clusters = {label: [] for label in unique_labels}
 
 # Group firms by cluster label
 for i, cluster_label in enumerate(cluster_labels):
-    clusters[cluster_label].append(firm_names[i])  # Use actual firm name instead of 'firm {i + 1}'
+    clusters[cluster_label].append(firm_names[i])
 
 # Print the clusters
 for cluster_label, firms in clusters.items():
@@ -31,7 +31,7 @@ for cluster_label, firms in clusters.items():
 
     # Plot the line graph for firms in the cluster
     for firm in firms:
-        firm_index = list(firm_names).index(firm)  # Get index of firm name in firm_names
+        firm_index = list(firm_names).index(firm)
         firm_data = data_array[firm_index]
 
         plt.plot(range(1, len(firm_data) + 1), firm_data, label=firm)
@@ -49,57 +49,3 @@ for cluster_label, firms in clusters.items():
     plt.show()
 
     print()
-
-
-'''
-import pandas as pd
-from sklearn.cluster import DBSCAN
-import matplotlib.pyplot as plt
-
-# Read data from CSV file
-data = pd.read_csv('../files/feature_set/1990-1.csv')
-data_array = data.values[:, 1:]  # Exclude the first column (firm names)
-
-# Define DBSCAN parameters
-eps = 80  # Maximum distance between two samples to be considered as neighbors
-min_samples = 3  # Minimum number of samples in a neighborhood for a point to be considered as a core point
-
-# Perform DBSCAN clustering
-dbscan = DBSCAN(eps=eps, min_samples=min_samples)
-cluster_labels = dbscan.fit_predict(data_array)
-
-# Get the unique cluster labels
-unique_labels = set(cluster_labels)
-
-# Create a dictionary to store firms in each cluster
-clusters = {label: [] for label in unique_labels}
-
-# Group firms by cluster label
-for i, cluster_label in enumerate(cluster_labels):
-    clusters[cluster_label].append(f'firm {i + 1}')
-
-# Print the clusters
-for cluster_label, firms in clusters.items():
-    print(f'Cluster {cluster_label}: {firms}')
-
-    # Plot the line graph for firms in the cluster
-    for firm in firms:
-        firm_index = int(firm.split()[1]) - 1  # Extract firm number from firm name
-        firm_data = data_array[firm_index]
-
-        plt.plot(range(1, len(firm_data) + 1), firm_data, label=firm)
-
-    plt.xlabel('Characteristics')
-    plt.ylabel('Data Value')
-    plt.title(f'Cluster {cluster_label}')
-
-    # List the firm names on the side of the graph
-    if len(firms) <= 10:
-        plt.legend(loc='center right')
-    else:
-        plt.legend(loc='center right', title=f'Total Firms: {len(firms)}', labels=firms[:10] + ['...'])
-
-    plt.show()
-
-    print()
-'''
--- codes_clustering/K-Means.py
+++ codes_clustering/K-Means.py
@@ -20,147 +20,10 @@ for k in k_values:
 
     clusters = {i: [] for i in range(k)}  # Dictionary key-value pair
     for i, cluster in enumerate(cluster_labels):
-        clusters[cluster].append(firm_names[i])  # Use actual firm name instead of 'firm {i + 1}'
-
-    clusters_k[k] = clusters
-
-# Print the clusters for each k value
-for k, clusters in clusters_k.items():
-    print(f'Clusters for k = {k}:')
-    for cluster, firms in clusters.items():
-        print(f'Cluster {cluster + 1}: {firms}')
-
-        # Plot the line graph for firms in each cluster
-        for firm in firms:
-            firm_index = list(firm_names).index(firm)  # Get index of firm name in firm_names
-            firm_data = data_array[firm_index]
-
-            plt.plot(range(1, len(firm_data) + 1), firm_data, label=firm)
-
-        plt.xlabel('Characteristics')
-        plt.ylabel('Data Value')
-        plt.title(f'Cluster {cluster + 1}, k={k}')
-
-        # List the firm names on the side of the graph
-        if len(firms) <= 10:
-            plt.legend(loc='center right')
-        else:
-            plt.legend(loc='center right', title=f'Total Firms: {len(firms)}', labels=firms[:10] + ['...'])
-
-        plt.show()
-
-    print()
-
-
-'''
-import pandas as pd
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Read data from CSV file
-data = pd.read_csv('../files/momentum/1990-01.csv')
-firm_names = data.values[:, 0]  # The first column contains firm names
-data_array = data.values[:, 1:].astype(float)  # Exclude the first column (firm names)
-
-# Separate firms with only NaN values
-nan_firms = np.where(np.isnan(data_array).all(axis=1))[0]
-valid_data = data_array[~np.isnan(data_array).all(axis=1)]
-
-# Define the number of clusters k
-k_values = [50]
-
-# Perform k-means clustering for each value of k
-clusters_k = {}
-
-for k in k_values:
-    kmeans = KMeans(n_clusters=k, n_init=10, random_state=0)  # n_init setting to suppress warning
-    kmeans.fit(valid_data)  # Compute k-means clustering
-    cluster_labels = kmeans.labels_  # Label of each point (ndarray of shape)
-
-    clusters = {i: [] for i in range(1, k + 1)}  # Dictionary key-value pair
-    for i, cluster in enumerate(cluster_labels):
-        clusters[cluster + 1].append(firm_names[i])
-
-    if len(nan_firms) > 0:
-        clusters[0] = [firm_names[i] for i in nan_firms]
-
-    clusters_k[k] = clusters
-
-# Print the clusters for each k value
-for k, clusters in clusters_k.items():
-    print(f'Clusters for k = {k}:')
-
-    # Print Cluster 0 first
-    if 0 in clusters:
-        print(f'Cluster 0: {clusters[0]}')
-
-    # Print other clusters
-    for cluster, firms in clusters.items():
-        if cluster == 0:
-            continue
-
-        print(f'Cluster {cluster}: {firms}')
-
-        # Plot the line graph for firms in each cluster
-        if cluster != 0:  # Exclude Cluster 0 from plotting
-            for firm in firms:
-                firm_index = np.where(firm_names == firm)[0][0]  # Find the index of the firm in the original data
-                firm_data = data_array[firm_index]
-
-                if not np.isnan(firm_data).all():  # Skip NaN firms for plotting
-                    plt.plot(range(1, len(firm_data) + 1), firm_data, label=firm)
-
-            plt.xlabel('Characteristics')
-            plt.ylabel('Data Value')
-            plt.title(f'Cluster {cluster}, k={k}')
-
-            # List the firm names on the side of the graph
-            if len(firms) <= 10:
-                plt.legend(loc='center right')
-            else:
-                plt.legend(loc='center right', title=f'Total Firms: {len(firms)}', labels=firms[:10] + ['...'])
-
-            plt.show()
-
-    print()
-'''
-
-'''
-import pandas as pd
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Read data from CSV file
-data = pd.read_csv('../files/feature_set/1990-1.csv')
-firm_names = data.values[:, 0]  # The first column contains firm names
-data_array = data.values[:, 1:].astype(float)  # Exclude the first column (firm names)
-
-# Separate firms with only NaN values
-nan_firms = np.where(np.isnan(data_array).all(axis=1))[0]
-valid_data = data_array[~np.isnan(data_array).all(axis=1)]
-
-# Define the number of clusters k
-k_values = [5]
-
-# Perform k-means clustering for each value of k
-clusters_k = {}
-
-for k in k_values:
-    kmeans = KMeans(n_clusters=k, n_init=10, random_state=0)  # n_init setting to suppress warning
-    kmeans.fit(valid_data)  # Compute k-means clustering
-    cluster_labels = kmeans.labels_  # Label of each point (ndarray of shape)
-
-    clusters = {i: [] for i in range(k)}  # Dictionary key-value pair
-    for i, cluster in enumerate(cluster_labels):
         clusters[cluster].append(firm_names[i])
 
     clusters_k[k] = clusters
 
-# Add the NaN firms as an additional cluster
-clusters_k[k][k] = [firm_names[i] for i in nan_firms]
-
 # Print the clusters for each k value
 for k, clusters in clusters_k.items():
     print(f'Clusters for k = {k}:')
@@ -169,66 +32,7 @@ for k, clusters in clusters_k.items():
 
         # Plot the line graph for firms in each cluster
         for firm in firms:
-            firm_index = np.where(firm_names == firm)[0][0]  # Find the index of the firm in the original data
-            firm_data = data_array[firm_index]
-
-            if not np.isnan(firm_data).all():  # Skip NaN firms for plotting
-                plt.plot(range(1, len(firm_data) + 1), firm_data, label=firm)
-
-        plt.xlabel('Characteristics')
-        plt.ylabel('Data Value')
-        plt.title(f'Cluster {cluster + 1}, k={k}')
-
-        # List the firm names on the side of the graph
-        if len(firms) <= 10:
-            plt.legend(loc='center right')
-        else:
-            plt.legend(loc='center right', title=f'Total Firms: {len(firms)}', labels=firms[:10] + ['...'])
-
-        plt.show()
-
-    print()
-'''
-
-'''
-import pandas as pd
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-
-
-# Performs K-Means clustering using SciKit-Learn in different number of cluster
-# Prints out the clusters for each value of k and creates a line graph for each cluster
-
-# Read data from CSV file
-data = pd.read_csv('../files/momentum_past/2004-01.csv')
-data_array = data.values[:, 1:]  # Exclude the first column (firm names)
-
-# Define the number of clusters k
-k_values = [50]
-
-# Perform k-means clustering for each value of k
-clusters_k = {}
-
-for k in k_values:
-    kmeans = KMeans(n_clusters=k, n_init=10, random_state=0)  # n_init setting to suppress warning
-    kmeans.fit(data_array)  # Compute k-means clustering
-    cluster_labels = kmeans.labels_  # Label of each point (ndarray of shape)
-
-    clusters = {i: [] for i in range(k)}  # Dictionary key-value pair
-    for i, cluster in enumerate(cluster_labels):
-        clusters[cluster].append(f'firm {i + 1}')
-
-    clusters_k[k] = clusters
-
-# Print the clusters for each k value
-for k, clusters in clusters_k.items():
-    print(f'Clusters for k = {k}:')
-    for cluster, firms in clusters.items():
-        print(f'Cluster {cluster + 1}: {firms}')
-
-        # PLot the line graph for firms in each cluster
-        for firm in firms:
-            firm_index = int(firm.split()[1]) - 1  # Extract firm number from firm name
+            firm_index = list(firm_names).index(firm)
             firm_data = data_array[firm_index]
 
             plt.plot(range(1, len(firm_data) + 1), firm_data, label=firm)
@@ -246,4 +50,3 @@ for k, clusters in clusters_k.items():
         plt.show()
 
     print()
-'''
