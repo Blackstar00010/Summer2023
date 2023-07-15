fe786126aaeca9cb2b5ab90c9f1052dbb986eec2
kimjuhwan1234 <asas4539@hanyang.ac.kr>
2023-07-15 19:55:36 +0900

    .

--- codes_clustering/Hierarchical Agglomerative_table.py
+++ codes_clustering/Hierarchical Agglomerative_table.py
@@ -20,13 +20,11 @@ def find_outliers_hac(threshold):
 
 # 2. CSV 파일 하나에 대해서 각각 실행
 for file in Hierarchical_Agglomerative:
-    print(file)
     first = True
     if first == True:
         data = read_and_preprocess_data(input_dir, file)
         # PCA 주성분 데이터만 가지고 있는 mat과 원본 Mom1을 추가로 가지고 있는 LS생성.
-        LS = data.values
-        mat = LS[:, 1:]
+        mat = data.values[:, 1:]
 
         # 4. Hierarchical Agglomerative 알고리즘 구현
         # 거리 행렬 계산
@@ -62,54 +60,9 @@ for file in Hierarchical_Agglomerative:
         for i, cluster_label in enumerate(clusters):
             clust[unique_labels.index(cluster_label)].append(data.index[i])
 
-        print(clusters)
-        print(clust)
+        for cluster_num, firms in enumerate(clust):
+            # Sort firms based on momentum_1
+            firms_sorted = sorted(firms, key=lambda x: data.loc[x, '1'])
+            print(firms_sorted)
 
-
-        #new_table_generate(LS, clust, output_dir, file)
-
-    # # 클러스터 내부에서 Frim Number 기준정렬 후 Rank와 Long Short Value계산하여result_matrix저장.
-    # result_matrix = []
-    # for key, value in list_dict.items():
-    #     row = [key]
-    #     sorted_data = sorted(value, key=lambda x: float(x.split(': ')[1]), reverse=True)
-    #     length = len(sorted_data)
-    #     # h = Rank, t = Long Short Value
-    #     for i, item in enumerate(sorted_data):
-    #         rank = i - length // 2
-    #         h = rank + (length + 1) // 2
-    #         t = 0 if length % 2 == 0 else (1 if rank > 0 else -1)
-    #         result = f"{item}: {t}: {h}"
-    #         row.append(result)
-    #     result_matrix.append(row)
-    # df = pd.DataFrame(result_matrix)
-    # # df의 각 원소에 대하여 맨 뒤에 소속 Cluster를 붙임.
-    # for i in range(0, 100):
-    #     for j in range(0, len(df.head(i + 1).iloc[i].dropna()) - 1):
-    #         df.iloc[i, j + 1] = str(df.iloc[i, j + 1]) + ':' + str(df.iloc[i, 0])
-    # df = df.iloc[:, 1:]
-    # # 결측값을 제거하고 pure data만 result_list에 저장
-    # result_list = []
-    # for row in df.values:
-    #     for item in row:
-    #         if pd.notnull(item):
-    #             result_list.append(item)
-    # # list 값 정렬 후 ":"기준 분리
-    # sorted_result_list = sorted(result_list, key=lambda x: int(x.split(":")[0][4:]))
-    # split_result_list = [item.split(":") for item in sorted_result_list]
-    #
-    # # 7. Result CSV 생성
-    # # Firm Number 대신에 실제 Firm이름으로 대체.
-    # output_file = os.path.join(output_dir, file)
-    # df_sorted = pd.DataFrame(split_result_list)
-    # df_sorted.columns = ["Firm", "Mom1", "LS", "Rank", "Cluster"]
-    # df_sorted = df_sorted.iloc[:, 1:]
-    # df_sorted.insert(0, 'Firm', firms_list)
-    # # 회사별로 정렬된 dataframe을 다시 Cluster에 대하여 정렬.
-    # df_sorted[df_sorted.columns[4]] = pd.to_numeric(df_sorted[df_sorted.columns[4]])
-    # df_sorted = df_sorted.sort_values(by=df_sorted.columns[4])
-    # df_sorted = df_sorted.sort_values(by=['Cluster', df_sorted.columns[3]])
-    #
-    # # 8. Print the download link and file path for the saved CSV file
-    # # df_sorted의 열은 "Firm", "Mom1", "LS", "Rank", "Cluster"
-    # df_sorted.to_csv(output_file, index=False)
+        new_table_generate(data, clust, output_dir, file)
