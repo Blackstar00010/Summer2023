7453508b1b6a34aca650689be74dd74d29ebd08e
kimjuhwan1234 <asas4539@hanyang.ac.kr>
2023-07-15 15:15:09 +0900

    Update PCA_table.py

--- codes_clustering/PCA_table.py
+++ codes_clustering/PCA_table.py
@@ -1,40 +1,41 @@
 from _table_generate import *
 from sklearn.decomposition import PCA
 
-# 1. 파일 불러오기
+# 파일 불러오기.
 input_dir = '../files/momentum'
 output_dir = '../files/Clustering/PCA'
 momentum = sorted(filename for filename in os.listdir(input_dir))
 
-# 2. PCA 알고리즘 구현
-def get_pca_data(data, n_components=2):
-    pca = PCA(n_components=n_components)
-    pca.fit(data)
-    return pca.transform(data), pca
+# 1. PCA 알고리즘 함수.
+first = True
+if first == True:
+    def get_pca_data(data, n_components):
+        pca = PCA(n_components=n_components)
+        pca.fit(data)
+        return pca.transform(data), pca
 
 
-def get_pd_from_pca(pca_data, cols=None):
-    if cols is None:
-        cols = ['pca_component_{}'.format(i + 1) for i in range(pca_data.shape[1])]
-    return pd.DataFrame(pca_data, columns=cols)
+    def get_pd_from_pca(pca_data, cols=None):
+        if cols is None:
+            cols = ['pca_component_{}'.format(i + 1) for i in range(pca_data.shape[1])]
+        return pd.DataFrame(pca_data, columns=cols)
 
 
-def print_variance_ratio(pca):
-    sum=np.sum(pca.explained_variance_ratio_)
-    # 'variance_ratio: ', pca.explained_variance_ratio_
-    # 'sum of variance_ratio: ', np.sum(pca.explained_variance_ratio_)
-    return sum
+    def variance_ratio(pca):
+        sum = np.sum(pca.explained_variance_ratio_)
+        return sum
 
-# 3. CSV 파일 하나에 대해서 각각 실행
+# CSV 파일 하나에 대해서 각각 실행.
 for file in momentum:
+
     data = read_and_preprocess_data(input_dir, file)
     mat = data.values
 
-    # 4. 최적 n_components 찾기
-    mc = True
-    if mc == True:
+    # 2. 최적 n_components 찾기
+    second = True
+    if second == True:
 
-        if len(data)<20:
+        if len(data) < 20:
             n_components = len(data)
 
         else:
@@ -43,43 +44,30 @@ for file in momentum:
         while True:
             pca = PCA(n_components)
             pca.fit(mat)
-            t = print_variance_ratio(pca)
+            t = variance_ratio(pca)
 
             if t < 0.99 or n_components < 2:
                 break
             else:
-                n_components-=1
+                n_components -= 1
 
-        pca = PCA(n_components+1)
+        pca = PCA(n_components + 1)
         pca.fit(mat)
-        t = print_variance_ratio(pca)
-        n_components=n_components+1
+        t = variance_ratio(pca)
+        n_components = n_components + 1
 
-        # print(t)
-        # print(n_components)
-
-    # 5. PCA 진행
-    md = True
-    if md == True:
+    # 3. PCA 진행 및 결과
+    third = True
+    if third == True:
         # get_pd_from_pca에 넣을 columns 생성
         cols = []
-        for i in range(1, n_components+1):
+        for i in range(1, n_components + 1):
             cols.append('pca_' + str(i))
 
         pca_mat, pca = get_pca_data(mat, n_components=n_components)
         pca_mat_pd = get_pd_from_pca(pca_mat, cols=cols)
         pca_x = pca_mat_pd[cols]
-        t= print_variance_ratio(pca)
-
-        # print(t)
-        # print(n_components)
-
-    # 6. Standardscaler 및 결과 확인
-    mk=True
-    if mk == True:
-
-        print("original shape: ", mat.shape)
-        print("transformed shape: ", pca_mat.shape)
+        t = variance_ratio(pca)
 
         # Standardscaler
         # 각 열의 평균과 표준편차 계산
@@ -91,23 +79,25 @@ for file in momentum:
         mat_pd_pca = get_pd_from_pca(pca_mat)
         mat_pd_pca.head()
         mat_pd_pca_matrix = mat_pd_pca.values
-        # 원본 data의 첫열
+
+        # Original Mom1 Combining
         first_column = data.iloc[:, 0]
         first_column_matrix = np.array(first_column).reshape(-1, 1)
         first_column_matrix = first_column_matrix[0:]
-        # 행렬 결합
         combined_matrix = np.hstack((first_column_matrix, mat_pd_pca_matrix))
+        df_combined = pd.DataFrame(combined_matrix)
 
+        # Result
         print(file)
+        print("original shape: ", mat.shape)
+        print("transformed shape: ", pca_mat.shape)
         print(t)
         print(mat_pd_pca)
-        df_combined = pd.DataFrame(combined_matrix)
         print(df_combined)
 
-    #결과 출력
-    third=True
-    if third==True:
-        # 6. Result CSV 생성
+    # 4. PCA 결과 CSV로 저장
+    forth = False
+    if forth == True:
         # Columns=['Original Mom1', 'data after PCA', ...]
         output_file = os.path.join(output_dir, file)
         df_combined = pd.DataFrame(combined_matrix)
