e6e3fb59706447ebacd76ac7434c4dbd4839cade
kimjuhwan1234 <asas4539@hanyang.ac.kr>
2023-07-16 16:09:48 +0900

    Update Gaussian Mixture Model.py

--- codes_clustering/Gaussian Mixture Model.py
+++ codes_clustering/Gaussian Mixture Model.py
@@ -17,6 +17,12 @@ def gmm_bic_score(estimator, X):
     return -estimator.bic(X)
 
 
+'''
+바꾼 이유
+GMM-EM을 사용하면 Cluster 수를 지정해줘야 하기 때문에 최적 Cluster 못찾음.
+BayesianGaussianMixture는 Cluster 수를 데이터로부터 알아서 추론.
+'''
+
 # 1. 최적 covariance 찾기
 param_grid = {
     "covariance_type": ["spherical", "tied", "diag", "full"],
@@ -42,23 +48,47 @@ min_row_covariance = df.iloc[min_row_index, 0]
 print(min_row_covariance)
 
 # 2. GMM
-dpgmm = mixture.BayesianGaussianMixture(n_components=10, covariance_type=min_row_covariance).fit(mat)
+dpgmm = mixture.BayesianGaussianMixture(n_components=40, covariance_type=min_row_covariance).fit(mat)
 
 cluster_labels = dpgmm.predict(mat)
 
-clusters = [[] for _ in range(10)]
+clusters = [[] for _ in range(40)]
+clusters2 = [[] for _ in range(40)]
 
 for i, cluster_num in enumerate(cluster_labels):
     clusters[cluster_num].append(data.index[i])
+    clusters2[cluster_num].append(i)
 
 clusters = [sublist for sublist in clusters if sublist]
+clusters2 = [sublist for sublist in clusters2 if sublist]
 
 unique_labels = []
 for _ in range(len(clusters)):
     unique_labels.append(_)
 
-# 3. Outlier(예정)
+# 3. Outlier(미완)
+threshold = 0.01
+outliers = []
 
-# 4. Print and plot the clusters
-for i, firms in enumerate(clusters):
-    plot_clusters(unique_labels[i], firms, data.index, mat)
+# 각 클러스터에서 이상치 판별
+for cluster in clusters2:
+    cluster_size = len(clusters2)
+    cluster_probabilities = dpgmm.predict_proba(mat[cluster])
+
+    # 클러스터에 속하는 각 데이터 포인트에 대해 확률 평균 계산
+    cluster_prob_mean = np.mean(cluster_probabilities, axis=0)
+    print(cluster_prob_mean)
+
+    # 클러스터의 데이터 포인트 중 확률 평균이 임계값보다 작은 것을 이상치로 판별
+    for i, prob_mean in enumerate(cluster_prob_mean):
+        print(i)
+        print(prob_mean)
+        # if prob_mean < threshold:
+        #     outliers.append(cluster[i])
+
+# 이상치(outlier) 리스트 출력
+print("Outliers:", outliers)
+
+# # 4. Print and plot the clusters
+# for i, firms in enumerate(clusters):
+#     plot_clusters(unique_labels[i], firms, data.index, mat)
