7ffa002db30fed12f2bf8a86f877b66b52076fbd
kimjuhwan1234 <asas4539@hanyang.ac.kr>
2023-07-31 15:06:36 +0900

    .

--- codes_clustering/dbscan_paper.py
+++ codes_clustering/dbscan_paper.py
@@ -2,8 +2,7 @@ import math
 from t_SNE import *
 from _Cluster_Plot import *
 from sklearn.cluster import DBSCAN
-from sklearn.metrics import silhouette_score
-from dbscan_checkcheck import successful_params
+from sklearn.neighbors import NearestNeighbors
 
 input_dir = '../files/PCA/PCA(1-48)'
 file = '2022-12.csv'
@@ -11,16 +10,39 @@ data = read_and_preprocess_data(input_dir, file)
 data_array = data.values[:, 1:].astype(float)
 firm_names = data.index
 
-eps_values = np.linspace(0.01, 3., 300)
-min_samples_values = range(2, 20)
+ms = int(math.log(len(data_array)))
+
+nbrs = NearestNeighbors(n_neighbors=ms + 1).fit(data_array)
+distances, indices = nbrs.kneighbors(data_array)
+
+# 각 데이터 포인트의 MinPts 개수의 최근접 이웃들의 거리의 평균 계산
+avg_distances = np.mean(distances[:, 1:], axis=1)
+
+# 결과 출력
+print("Average distances to the nearest MinPts neighbor points:")
+print(avg_distances)
 
 
-def perform_DBSCAN2(data_array):
+def perform_DBSCAN(data_array):
+    ms = int(math.log(len(data_array)))
 
-    ms=math.log(len(data_array))
+    nbrs = NearestNeighbors(n_neighbors=ms + 1).fit(data)
+    distances, indices = nbrs.kneighbors(data)
+
+    # 각 데이터 포인트의 MinPts 개수의 최근접 이웃들의 거리의 평균 계산
+    avg_distances = np.mean(distances[:, 1:], axis=1)
+    avg_distances = sum(avg_distances) / len(data_array)
+
+    eps = 0.4 * avg_distances
+
+    print(ms)
+    print(eps)
 
     lab = True
     if lab:
+        # 결과 출력
+        print("Average distances to the nearest MinPts neighbor points:")
+        print(avg_distances)
 
         labels = DBSCAN(min_samples=ms, eps=eps, metric='manhattan').fit(data_array).labels_
         dbscan = DBSCAN(min_samples=ms, eps=eps, metric='manhattan')
@@ -29,17 +51,17 @@ def perform_DBSCAN2(data_array):
 
 
 if __name__ == "__main__":
-    labels, dbscan = perform_DBSCAN2(data_array, successful_params)
+    labels, dbscan = perform_DBSCAN(data_array)
 
     # Get the unique cluster labels
     unique_labels = sorted(list(set(labels)))
 
     clust = [[] for _ in unique_labels]
     for i, cluster_label in enumerate(labels):
-       clust[unique_labels.index(cluster_label)].append(data.index[i])
+        clust[unique_labels.index(cluster_label)].append(data.index[i])
 
     # 3. Print and plot the clusters
     for i, firms in enumerate(clust):
         plot_clusters(unique_labels[i], firms, data.index, data_array)  # Use the imported function
 
-    t_SNE(data_array, dbscan)
+    # t_SNE(data_array, dbscan)
