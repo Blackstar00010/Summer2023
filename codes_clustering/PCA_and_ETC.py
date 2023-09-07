4c0dd2a8d8880c0307c4acb446dad587d092bf2a
ovadaprofit <110870807+ovadaprofit@users.noreply.github.com>
2023-09-07 11:22:14 +0900

    ETC

--- codes_clustering/PCA_and_ETC.py
+++ codes_clustering/PCA_and_ETC.py
@@ -2,17 +2,12 @@ import os
 import warnings
 import numpy as np
 import pandas as pd
-import statsmodels.api as sm
 import matplotlib.pyplot as plt
-from joblib import Parallel, delayed
 from sklearn import metrics
 from sklearn import manifold
-from itertools import combinations
-from sklearn.cluster import *
-from sklearn.decomposition import PCA
-from statsmodels.tsa.stattools import coint, kpss
-from sklearn.model_selection import GridSearchCV
 from sklearn.mixture import *
+from sklearn.decomposition import PCA
+from sklearn.model_selection import GridSearchCV
 
 # turn off warning
 warnings.filterwarnings("ignore")
@@ -123,6 +118,7 @@ def save_and_plot_result(clustering_name, result_df: pd.DataFrame, file_names, F
     result_df = result_df.astype(float)  # set data type as float(df.value was str actually.)
     result_df = result_df.fillna(0)
 
+
     result_df.T.describe().to_csv(os.path.join('../files/result/', f'{clustering_name}_statistcs_original.csv'),
                                   index=True)
     result_df.to_csv(os.path.join('../files/result/', f'{clustering_name}_result_original.csv'), index=True)
@@ -132,14 +128,31 @@ def save_and_plot_result(clustering_name, result_df: pd.DataFrame, file_names, F
 
     # transform into log scale
     result_df.iloc[:, 0:] = np.log(result_df.iloc[:, 0:]) if apply_log else result_df.iloc[:, 0:]
-    criterion = np.log(2) if apply_log else 2
 
+    result_df.to_csv('../files/before.csv')
+    # drop irrational data ( larger than criterion )
+    criterion = np.log(1.5) if apply_log else 1.5
     result_df = result_df.applymap(lambda x: float('NaN') if abs(x) > criterion else x)
     result_df = result_df.applymap(
         lambda x: float('NaN') if x < 1 - 1 / criterion else x) if not apply_log else result_df
     result_df = result_df.fillna(method='ffill', axis=1)
 
+<<<<<<< HEAD
+    result_df.to_csv('../files/after.csv')
+    # dsjfksdlfjsad
+    result_modified = pd.DataFrame(
+        index=['count', 'annual return mean', 'annual return std', 'monthly return min', 'monthly return max'],
+        columns=result_df.index)
+    for i in range(len(result_modified.columns)):
+        result_modified.iloc[0, i] = len(result_df.columns)
+        result_modified.iloc[1, i] = np.exp(np.mean(result_df.iloc[i, :]) * 12) - 1
+        result_modified.iloc[2, i] = np.exp(np.std(result_df) * np.sqrt(12)) - 1
+        result_modified.iloc[3, i] = np.min(result_df.iloc[i, :])
+        result_modified.iloc[4, i] = np.max(result_df.iloc[i, :])
+    # result_modified.iloc[1, :] = result_modified.iloc[1, :] * len(result_df.iloc[1, :]) / 12
+=======
     sharpe_ratio = pd.DataFrame(index=['Sharpe ratio'], columns=result_df.index)
+>>>>>>> parent of 4787a445 (Merge branch 'main' of https://github.com/Blackstar00010/Summer2023)
 
     for i in range(len(result_df.index)):
         for j in range(1):
@@ -364,202 +377,6 @@ def find_optimal_GMM_covariance_type(data):
     best_covariance_type = grid_search.best_params_["covariance_type"]
     return best_covariance_type
 
-
-def find_optimal_HDBSCAN_min_cluster_size(data):
-    Hdbscan = HDBSCAN(allow_single_cluster=True)
-    # 탐색할 covariance type과 n_components 설정
-    param_grid = {"min_cluster_size": [2, 3, 4, 5, 6, 7]}
-
-    # BIC score를 평가 지표로 하여 GridSearchCV 실행
-    grid_search = GridSearchCV(Hdbscan, param_grid, scoring='neg_negative_likelihood_ratio')
-    grid_search.fit(data)
-
-    # 최적의 covariance type과 n_components 출력
-    best_min_cluster_size = grid_search.best_params_["min_cluster_size"]
-    return best_min_cluster_size
-
-
-if True:
-    def read_mom_data(data):
-        # mom1 save and data Normalization
-        mom1 = data.values.astype(float)[:, 0]
-        data_normalized = (data - data.mean()) / data.std()
-        mat = data_normalized.values.astype(float)
-
-        # mom1을 제외한 mat/PCA(2-49)
-        # mat = np.delete(mat, 0, axis=1)
-
-        # mom49를 제외한 mat/PCA(1-48)
-        mat = np.delete(mat, 48, axis=1)
-
-        df_combined = pd.DataFrame(mat)
-        df_combined.insert(0, 'Mom1', mom1)
-        df_combined.index = data.index
-
-        return df_combined.T
-
-
-    def cointegrate(data, s1, s2):
-        x = data[s1].values
-        y = data[s2].values
-        _, p_value, _ = coint(x, y)
-        return p_value
-
-
-    def adf_result(pair):
-        try:
-            ret = sm.tsa.adfuller(pair)[1]
-        except ValueError:
-            ret = 0.06
-        return ret
-
-
-    def kpss_result(pair):
-        try:
-            ret = kpss(pair)[1]
-        except:
-            ret = 0.04
-        return ret
-
-
-    def find_cointegrated_pairs(data: pd.DataFrame) -> list:
-        n_jobs = 8
-        data = data.iloc[1:, :]
-        pairs = pd.DataFrame(combinations(data.columns, 2))  # 모든 회사 조합
-        pairs['pvalue'] = Parallel(n_jobs=n_jobs)(delayed(cointegrate)(data, pair[0], pair[1]) for pair in pairs.values)
-        pairs = pairs.loc[pairs.index[pairs['pvalue'] < 0.01], :]
-        print('Finished filtering pairs using pvalue!')
-
-        spread_df: pd.DataFrame = pairs.apply(lambda x: data[x[0]] - data[x[1]], axis=1)
-        # spread_df['adf_result'] = Parallel(n_jobs=n_jobs)(delayed(adf_result)(pair) for pair in spread_df.values)
-        # spread_df = spread_df.loc[spread_df.index[spread_df['adf_result'] < 0.05], :]
-        # print('Finished filtering pairs using adf_result!')
-
-        spread_df['kpss_result'] = Parallel(n_jobs=n_jobs)(delayed(kpss_result)(pair) for pair in spread_df.values)
-        spread_df = spread_df.loc[spread_df.index[spread_df['kpss_result'] > 0.05], :]
-        spread_df = spread_df.drop(columns=['adf_result'])
-        spread_df = spread_df.drop(columns=['kpss_result'])
-        print('Finished filtering pairs using kpss_result!')
-
-        spread_sr = spread_df.iloc[:, 0]
-        pairs['spread'] = (spread_sr - spread_sr.mean()) / spread_sr.std()
-        pairs = pairs.dropna(subset=['spread'])
-        pairs = pd.DataFrame(pairs)
-        pairs = pairs.loc[pairs.index[pairs['spread'].abs() > 2], :]
-        pairs['pair1'] = pairs[0] * (pairs['spread'] > 0) + pairs[1] * (pairs['spread'] <= 0)
-        pairs['pair2'] = pairs[0] * (pairs['spread'] <= 0) + pairs[1] * (pairs['spread'] > 0)
-        pairs = pairs.drop(columns=[0, 1])
-        print('Finished filtering pairs using normalised spread!')
-
-        pairs.sort_values(by='pvalue', axis=0, inplace=True)
-        pairs = pairs.drop_duplicates(subset='pair1')
-        pairs = pairs.drop_duplicates(subset='pair2')
-        invest_list = pd.DataFrame(pairs.values.tolist())
-        invest_list = invest_list.iloc[:, 2:4]
-        invest_list = invest_list.values.tolist()
-
-        return invest_list
-
-
-    def find_cointegrated_pairs_deprecated(data: pd.DataFrame):
-        """
-        Deprecated
-        :param data:
-        :return:
-        """
-        data = data.iloc[1:, :]
-        invest_list = []
-
-        pairs = list(combinations(data.columns, 2))  # 모든 회사 조합
-        print(len(pairs))
-        pairs_len = 1
-
-        count_p = 0
-        count_s = 0
-        count_n = 0
-
-        while len(pairs) != pairs_len:
-            pairs_len = len(pairs)
-
-            for i, pair in enumerate(pairs):
-                pvalue = cointegrate(data, pair[0], pair[1])
-
-                if pvalue > 0.01:
-                    continue
-
-                else:
-                    count_p += 1
-                    spread = data[pair[0]] - data[pair[1]]
-                    adf_result = sm.tsa.adfuller(spread)
-                    kpss_result = kpss(spread)
-
-                    if adf_result[1] > 0.05 or kpss_result[1] < 0.05:
-                        continue
-
-                    else:
-                        count_s += 1
-                        mean_spread = spread.mean()
-                        std_spread = spread.std()
-                        z_score = (spread - mean_spread) / std_spread
-
-                        spread_value = float(z_score[0])
-
-                        if abs(spread_value) <= 2:
-                            continue
-
-
-                        elif spread_value < -2:
-                            pair = (pair[1], pair[0])
-                            # pair = (pair[1], pair[0], pvalue, adf_result[1], kpss_result[1], spread_value)
-                            invest_list.append(pair)
-                            pairs = [p for p in pairs if all(item not in pair for item in p)]
-                            count_n += 1
-
-                            break
-
-                        elif spread_value > 2:
-                            pair = (pair[0], pair[1])
-                            # pair = (pair[0], pair[1], pvalue, adf_result[1], kpss_result[1], spread_value)
-                            invest_list.append(pair)
-                            pairs = [p for p in pairs if all(item not in pair for item in p)]
-                            count_n += 1
-
-                            break
-
-            print(len(pairs))
-            print(len(invest_list))
-
-        print(f'pvalue {count_p}')
-        print(f'stationary {count_s}')
-        print(f'final {count_n}')
-
-        return invest_list
-
-
-    def save_cointegrated_LS(output_dir, file, mom_data, invest_list):
-        LS_table = pd.DataFrame(columns=['Firm Name', 'Momentum_1', 'Long Short', 'Cluster Index'])
-
-        for cluster_num, firms in enumerate(invest_list):
-            # Sort firms based on momentum_1
-            long_short = [0] * 2
-            long_short[0] = -1
-            long_short[1] = 1
-            # Add the data to the new table
-            for i, firm in enumerate(firms):
-                LS_table.loc[len(LS_table)] = [firm, mom_data.T.loc[firm, 'Mom1'], long_short[i], cluster_num]
-
-        firm_list_after = list(LS_table['Firm Name'])
-        firm_list_before = list(mom_data.T.index)
-        Missing = [item for item in firm_list_before if item not in firm_list_after]
-
-        for i, firm in enumerate(Missing):
-            LS_table.loc[len(LS_table)] = [firm, mom_data.T.loc[firm, 'Mom1'], 0, -1]
-
-        LS_table.sort_values(by='Cluster Index', inplace=True)
-
-        # Save the output to a CSV file in the output directory
-        LS_table.to_csv(os.path.join(output_dir, file), index=False)
-        print(output_dir)
 
 # PCA_Result Check
 if __name__ == "__main__":
